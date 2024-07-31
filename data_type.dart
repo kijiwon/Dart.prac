66adaabd625af38c4c7eae9610450856714521df
kijiwon <kiji1313@gmail.com>
2024-07-31 18:52:07 +0900

    데이터 타입 - 자료형

---
+++ data_type.dart
@@ -0,0 +1,18 @@
+Dart의 자료형
+
+Dart에서 자료형은 모두 객체
+String, int, double, bool -> class
+따라서 import할 필요 없이 해당 자료형이 가지는 모든 method를 사용할 수 있음
+
+String name = 'kiji';
+bool alive = true;
+int age = 10;
+double money = 10.99;
+
+num -> int, double 둘 다 될 수 있을 떄 사용
+num x = 12;
+x = 1.2;
+
+Dart의 거의 전부가 객체로 이루어져 있음
+=> 진정한 객체 지향 언어로 불림
+
