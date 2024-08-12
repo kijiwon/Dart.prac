4ad1e85152e91561e87db9ab8c0979cea4b14bfb
kijiwon <kiji1313@gmail.com>
2024-08-12 23:16:21 +0900

    class - Constructors 방법2

--- class/class_constructors.dart
+++ class/class_constructors.dart
@@ -1,13 +1,21 @@
 class Player {
   // 변수 선언은 했지만 값은 나중에 받아오기
-  late final String name;
-  late int xp;
+  // late final String name;
+  // late int xp;
+
+// 방법 2
+  final String name;
+  int xp;
 
   // dart에서 생성자 함수는 클래스 이름과 같아야함
-  Player(String name, int xp) {
-    this.name = name;
-    this.xp = xp;
-  }
+  // Player(String name, int xp) {
+  //   this.name = name;
+  //   this.xp = xp;
+  // }
+
+  // 방법 2
+  // 받아온 파라미터를 각각 this.name, this.xp로 받기
+  Player(this.name, this.xp);
 
   void sayHello() {
     print('Hi my name is $name');
